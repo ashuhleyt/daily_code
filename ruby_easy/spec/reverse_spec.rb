48b64075a13823ece3aa6a9cf75a2c6f77ab9233
Ashley <ashuhleyt@gmail.com>
2023-10-06 17:33:15 -0700

    finished testing

--- ruby_easy/spec/reverse_spec.rb
+++ ruby_easy/spec/reverse_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe Reverse do
 
     expect(@rev.special_reverse(sentence, char)).to eq("word sehcraes are repus fun")
   end
+
+  it 'reverses the word in the string given the char' do 
+    sentence = "first man to walk on the moon"
+    char = "m"
+
+    expect(@rev.special_reverse(sentence, char)).to eq("first nam to walk on the noom")
+  end
+
+  it 'reverses the word in the string given the char' do 
+    sentence = "peter piper picked pickled peppers"
+    char = "p"
+
+    expect(@rev.special_reverse(sentence, char)).to eq("retep repip dekcip delkcip sreppep")
+  end
 end
